e480006d17f188b620debf2dc29fb3e3c506c165
Noah Kim <nk8na@virginia.edu>
2017-05-15 14:31:59 -0400

    works on noahdkim.com

---
+++ attila.js
@@ -0,0 +1,12 @@
+var input = document.getElementById("name");
+input.onkeyup = function() {check_for_backticks()};
+function evaluate_expression(match, p1, offset, string){
+    return p1;
+}
+function check_for_backticks(){
+    if (input && input.value) {
+        var newstr = (input.value).replace(/`([^`]*)`/, evaluate_expression);
+        input.value = newstr;
+    }
+}
+
