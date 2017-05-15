2ae540d110bb899980edc4c300625db1b2118c48
Noah Kim <nk8na@virginia.edu>
2017-05-15 15:29:33 -0400

    doesn't work on messenger

--- attila.js
+++ attila.js
@@ -1,4 +1,14 @@
-var input = document.getElementById("name");
+if (window.location.href.indexOf("noahdkim.com") > -1) {
+    var input = document.getElementById("name");
+}
+// This doesn't work.
+else if(window.location.href.indexOf("messenger.com") > -1){
+    var input_nodes = document.querySelector("[data-text=true]");
+    //var input = input_nodes[0];
+    alert(input_nodes);
+}
+    
+
 input.onkeyup = function() {check_for_backticks()};
 function evaluate_expression(match, p1, offset, string){
     return eval(p1);
