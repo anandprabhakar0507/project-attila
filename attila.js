3b4a674102aa72f6c88e6b9f5f8983610b1db5c1
Ian Fisher <ianfisher45@gmail.com>
2017-05-15 20:06:33 -0400

    Fix errors in the text handling code
    The extension now works for basic use cases!

--- attila.js
+++ attila.js
@@ -1,38 +1,14 @@
-/*
-if (window.location.href.indexOf("noahdkim.com") > -1) {
-    var input = document.getElementById("name");
-}
-// This doesn't work.
-else if(window.location.href.indexOf("messenger.com") > -1){
-    var input_nodes = document.querySelector("[data-text=true]");
-    //var input = input_nodes[0];
-    alert(input_nodes);
-}
-
-*/
-
 var walker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT, null, false);
 
 while (node = walker.nextNode()) {
-    node.textContent = substituteBackticks(node);
-    console.log(node);
+    handleTextNode(node);
+    //console.log(node);
 }
 
-function substituteBackticks(textNode) {
-    return textNode.nodeValue.replace(/`([^`]*)`/, function(match, p1, offset, string) { return eval(p1); });
+function handleTextNode(textNode) {
+    textNode.nodeValue = substituteBackticks(textNode.nodeValue);
 }
 
-/*
-input.onkeyup = function() { check_for_backticks() };
-
-function evaluate_expression(match, p1, offset, string){
-    return eval(p1);
+function substituteBackticks(text) {
+    return text.replace(/`([^`]*)`/, function(match, p1, offset, string) { return eval(p1); });
 }
-
-function check_for_backticks() {
-    if (input && input.value) {
-        var newstr = (input.value).replace(/`([^`]*)`/, evaluate_expression);
-        input.value = newstr;
-    }
-}
-*/
