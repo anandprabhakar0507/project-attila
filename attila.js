d3bcaf206b061a70757cd3427e547fbdf8534dc7
Ian Fisher <ianfisher45@gmail.com>
2017-05-17 21:43:58 -0400

    Change project structure to use document.activeElement
    This way we don't have to constantly scan the element tree to find all
    editable nodes, because we have direct access to the one that the
    browser knows the user is typing into. Unfortunately there are still
    some kinks to work out.

--- attila.js
+++ attila.js
@@ -1,79 +1,17 @@
-var editableNodes = [];
-function walk()
-{
-    rootNode = document.body;
-    console.log("walk");
-    // Find all the text nodes in rootNode
-    var walker = document.createTreeWalker(
-        rootNode,
-        NodeFilter.SHOW_TEXT,
-        null,
-        false
-    ),
-    node;
-
-    // Modify each text node's value
-    while (node = walker.nextNode()) {
-	if(isEditableNode(node)){
-	    editableNodes.push(node);
-	    handleText(node);
-	}
+document.onkeyup = function() {
+    if (document.activeElement.tagName != "BODY") {
+        handleText(document.activeElement);
     }
 }
-function walk2()
-{
-    console.log("walk2");
-    size = editableNodes.length;
-    for(i=0; i<size; i++){
-	handleText(editableNodes[i]);
-    }
 
-
-
-}
 function handleText(textNode) {
-  textNode.nodeValue = replaceText(textNode.nodeValue);
+    console.log(textNode);
+    if (textNode.nodeValue) {
+        textNode.nodeValue = replaceText(textNode.nodeValue);
+    }
 }
 
 function replaceText(v) {
     var pattern = /`([()+*/0-9\s-]+)`/;
-    console.log(v);
-    if(v.search(pattern) >-1){
-        v = v.replace(pattern, function(match, p1, offset, string) { return eval(p1); });
-    }
-    return v;
-}
-
-// Marks editable content
-function isEditableNode(node) {
-    return node.isContentEditable || // DraftJS and many others
-    (node.parentNode && node.parentNode.isContentEditable) || // Special case for Gmail
-    (node.tagName && (node.tagName.toLowerCase() == "textarea" || // Some catch-alls
-                     node.tagName.toLowerCase() == "input"));
-}
-
-// Walk the doc (document) body, replace the title, and observe the body and title
-function walkAndObserve(doc) {
-    var observerConfig = {
-        characterData: true,
-        childList: true,
-        subtree: true
-    },
-	bodyObserver;
-
-    // Do the initial text replacements in the document body
-    walk();
-    
-    // Observe the body so that we replace text in any added/modified nodes
-   
-   // bodyObserver = new MutationObserver(observerCallback);
-    //bodyObserver.observe(doc.body, observerConfig);
-
-}
-
-// We want keyup instead of keydown but I can't figure out how to make it work
-document.onkeyup =  walk2;
-walk();
-function test(){
-    console.log("Test function");
+    return v.replace(pattern, function(match, p1, offset, string) { return eval(p1); });
 }
