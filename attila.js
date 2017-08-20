fcfa0f6e20b7d2c20eb5ddb44283a98f8804f751
Noah Kim <nk8na@virginia.edu>
2017-08-20 18:15:10 -0400

    Functional calculator

--- attila.js
+++ attila.js
@@ -1,21 +1,58 @@
 var inBackticks = false;
-$(document.activeElement).on("keyup", function(event) {
-    if (event.which === 192) {
-        if (inBackticks) {
-            var active = $(document.activeElement);
-            if (active.prop("tagName") === "INPUT") {
-                active.val(replaceText(active.val()));
-            } else if (active.prop("tagName") !== "BODY") {
-                active.text(replaceText(active.text()));
-            }
-            inBackticks = false;
-        } else {
-            inBackticks = true;
-        }
-    }
-});
-
+var characterToFind = "`"
 var pattern = /`([()*+/0-9\s-]+)`$/;
-function replaceText(v) {
-    return v.replace(pattern, function(match, p1, offset, string) { return eval(p1); });
+var expressionArray=[];
+var expression;
+$("body").prepend ( `
+  <style>
+         #calculator { width: 33%; height: 10%; padding: 0.5em; background:#eee; z-index:100000;}
+         #expression { float: left; width: 90%}
+         #answer { float: right; width: 10%}
+  </style>
+  <div id="calculator" class="ui-widget-content">
+    <div id="expression">
+      1+1
+    </div>
+    <div id="answer">
+      2
+    </div>
+  </div>
+` );
+$( function() {
+    $( "#calculator" ).draggable().css("position", "absolute");
+  } );
+var calcDiv = document.getElementById("calculator");
+var answerDiv = document.getElementById("answer");
+var expressionDiv = document.getElementById("expression");
+hideCalcDiv();
+$(document.activeElement).on("keyup", function(event) {
+  if (equationExists()){
+    showCalcDiv();
+  }
+  else{
+    hideCalcDiv();
+  }
+});
+function equationExists(){
+  console.log('check');
+  var active = $(document.activeElement);
+  var activeText = active.text();
+  expressionArray = activeText.match(pattern);
+  if(expressionArray!=null){
+    console.log("hello")
+    expression = expressionArray[0];
+  }
+  return expressionArray!=null;
+}
+
+function showCalcDiv(){
+  expression = expression.substring(1, expression.length-1);
+  expressionDiv.innerHTML = expression;
+  answerDiv.innerHTML = "=" + eval(String(expression));
+  console.log()
+  calcDiv.style.display = 'block';
+}
+
+function hideCalcDiv(){
+  calcDiv.style.display = 'none';
 }
